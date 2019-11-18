c52e3f9530c67c000d3500808f725634848f4b7f
txagg1992 <curiousaps@gmail.com>
2019-11-18 17:35:35 -0600

    Add Interfaces Class.

--- biometriclibrary/src/main/java/com/curiousca/biometriclibrary/BiometricCallback.kt
+++ biometriclibrary/src/main/java/com/curiousca/biometriclibrary/BiometricCallback.kt
@@ -2,4 +2,23 @@ package com.curiousca.biometriclibrary
 
 interface BiometricCallback {
 
+    fun onSDKVersionNotSupported()
+
+    fun onBiometricAuthenticationNotSupported()
+
+    fun onBiometricAuthenticationNotAvailable()
+
+    fun onBiometricAuthenticationPermissionNotGranted()
+
+    fun onBiometricAuthenticationInternalError(error: String)
+
+    fun onAuthenticationFailed()
+
+    fun onAuthenticationCancelled()
+
+    fun onAuthenticationSuccessful()
+
+    fun onAuthenticationHelp(helpCode: Int, helpString: String)
+
+    fun onAuthenticationError(errorCode: Int, errString: String)
 }
