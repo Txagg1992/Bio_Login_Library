9f799a9258985178f444e2e9771bedbeb96c994d
Txagg1992 <curiousaps@gmail.com>
2019-11-29 15:31:07 -0600

    Just a toast.

--- app/src/main/java/com/curiousca/bio_library/MainActivity.kt
+++ app/src/main/java/com/curiousca/bio_library/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.curiousca.bio_library
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Ooyaah feeling dangerous", Toast.LENGTH_SHORT).show()
     }
 }
